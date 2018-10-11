3f3c841c9511e61dda6383b63824039692e0d2c7
josieperalta <josieperalta@joezsmac.home>
2018-10-10 20:37:44 -0400

    generate random gif url

--- src/App.js
+++ src/App.js
@@ -9,21 +9,19 @@ class App extends Component {
     super(props)
     this.state = {
       url: "",
-      height: "",
-      width:""
-      //embed_url
+
+      //original_url
     }
   }
 
 handleClick(){
-  let url = 'https://api.giphy.com/v1/gifs/search?api_key=Gs5LTAuwlKWcKL8jl599o8T1Q1X9V1x7&q=giphy&limit=25&offset=0&rating=G&lang=en'
+  let url = 'https://api.giphy.com/v1/gifs/random?api_key=Gs5LTAuwlKWcKL8jl599o8T1Q1X9V1x7&tag=&rating=G'
   fetch(url)
     .then( res => res.json() )
     .then( data => {
     this.setState(prevState => ({
-      url: data.data[0].images.original.url,
-      height: data.data[0].images.original.height,
-      width: data.data[0].images.original.width
+      url: data.data.image_original_url,
+
       }))
     })
 
@@ -36,7 +34,7 @@ handleClick(){
       <div className="Gif-App">
         <h1> Giphy Generator </h1>
         <button onClick = { () => this.handleClick()}>Random GIF</button>
-        <Gif/>
+        <Gif url={this.state.url}/>
       </div>
     );
   }
