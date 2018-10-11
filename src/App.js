ea56e778fb797c2490d78ff7af22eb46929eb594
josieperalta <josieperalta@joezsmac.home>
2018-10-10 20:02:15 -0400

    added components and jsx

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Gif from './components/Gif'
+
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      gif: ""
+      url: "",
+      height: "",
+      width:""
+      //embed_url
     }
   }
 
-  gifApi(){
-    let url = "https://api.giphy.com/v1/gifs/search?api_key=Gs5LTAuwlKWcKL8jl599o8T1Q1X9V1x7&q=giphy&limit=25&offset=0&rating=G&lang=en"
-    fetch(url)
-      .then( res => res.json() )
-      .then( data => {
-        this.setState(prevState => ({
-          gif: data.data
-        }))
-      })
-  }
-
-handleSubmit(){
-
-}
-
-handleChange(){
+handleClick(){
+  let url = 'https://api.giphy.com/v1/gifs/search?api_key=Gs5LTAuwlKWcKL8jl599o8T1Q1X9V1x7&q=giphy&limit=25&offset=0&rating=G&lang=en'
+  fetch(url)
+    .then( res => res.json() )
+    .then( data => {
+    this.setState(prevState => ({
+      url: data.data[0].images.original.url,
+      height: data.data[0].images.original.height,
+      width: data.data[0].images.original.width
+      }))
+    })
 
 }
+
 
   render() {
+    console.log(this.state)
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Gif-App">
+        <h1> Giphy Generator </h1>
+        <button onClick = { () => this.handleClick()}>Random GIF</button>
+        <Gif/>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/components/Gif.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Gif extends Component{
+  render(){
+    // let url = {this.props}
+    return(
+      <div>
+        <iframe>
+        </iframe>
+      </div>
+      )
+  }
+}
+
+export default Gif
